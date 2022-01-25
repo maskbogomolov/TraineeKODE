d46f37b0c1ced30a226231cef4e0a80bb34316f5
nypro <nyprosto111@gmail.com>
2022-01-25 21:52:41 +0300

    added ConnectivityManager

--- app/src/main/java/com/example/appkode/presentation/UsersListFragment.kt
+++ app/src/main/java/com/example/appkode/presentation/UsersListFragment.kt
@@ -7,24 +7,24 @@ import androidx.fragment.app.Fragment
 import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.appkode.R
 import com.example.appkode.databinding.FragmentUsersListBinding
 import com.example.appkode.di.appComponent
-import com.example.appkode.util.Data
-import com.example.appkode.util.NetworkResponse
 import dagger.Lazy
 import javax.inject.Inject
 
-
+//Toast.makeText(requireContext(),"2",Toast.LENGTH_SHORT).show()
 class UsersListFragment : Fragment(), SearchView.OnQueryTextListener {
 
     @Inject
     lateinit var usersViewModelFactory: Lazy<UsersViewModel.Factory>
     private val viewModel: UsersViewModel by viewModels { usersViewModelFactory.get() }
     private val usersAdapter: UsersAdapter by lazy { UsersAdapter(requireActivity(), viewModel) }
-    private var _binding : FragmentUsersListBinding? = null
+    private var _binding: FragmentUsersListBinding? = null
     private val binding get() = _binding!!
+
 
     override fun onAttach(context: Context) {
         context.appComponent.inject(this)
@@ -35,19 +35,26 @@ class UsersListFragment : Fragment(), SearchView.OnQueryTextListener {
         super.onCreate(savedInstanceState)
         arguments?.takeIf { it.containsKey("getDepartment") }?.apply {
             viewModel.department.value = getString("getDepartment").toString()
-
         }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        _binding = FragmentUsersListBinding.inflate(inflater,container,false)
+        _binding = FragmentUsersListBinding.inflate(inflater, container, false)
         binding.usersList.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = usersAdapter
         }
 
+
         viewModel.filter.observe(viewLifecycleOwner) {
-            usersAdapter.submitList(it)
+            if (it.isNotEmpty()) {
+                usersAdapter.submitList(it)
+            } else if (it.isEmpty() && !viewModel.isOnline(requireContext())){
+                findNavController().navigate(R.id.errorFragment)
+            }else{
+                Toast.makeText(requireContext(),"3",Toast.LENGTH_SHORT).show()
+                viewModel.getUser()
+            }
         }
         setHasOptionsMenu(true)
         return binding.root
@@ -66,18 +73,9 @@ class UsersListFragment : Fragment(), SearchView.OnQueryTextListener {
 
     override fun onQueryTextChange(newText: String?): Boolean {
         if (newText != null) {
-            Toast.makeText(requireContext(), "$newText", Toast.LENGTH_SHORT).show()
             viewModel.queryFlow.value = newText
-            viewModel.filter.observe(viewLifecycleOwner) {
-                if (it.isEmpty()) {
-                    usersAdapter.submitList(emptyList())
-                    binding.usersList.visibility = View.INVISIBLE
-                    binding.holderErrorMessage.visibility = View.VISIBLE
-                } else
-                    binding.usersList.visibility = View.VISIBLE
-                usersAdapter.submitList(it)
-            }
         }
         return true
     }
+
 }
--- app/src/main/java/com/example/appkode/presentation/UsersViewModel.kt
+++ app/src/main/java/com/example/appkode/presentation/UsersViewModel.kt
@@ -1,42 +1,44 @@
 package com.example.appkode.presentation
 
-import android.util.Log
-import android.widget.Toast
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.content.ContextCompat.getSystemServiceName
 import androidx.lifecycle.*
-import com.example.appkode.data.database.UsersEntity
-
-import com.example.appkode.domain.User
 import com.example.appkode.domain.UsersRepository
-import com.example.appkode.util.Data
-import com.example.appkode.util.NetworkResponse
-import com.example.appkode.util.SortOrder
+import com.example.appkode.util.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Provider
 
-class UsersViewModel(val repository: UsersRepository): ViewModel() {
+class UsersViewModel(val repository: UsersRepository) : ViewModel() {
 
     val sortOrder = MutableStateFlow(SortOrder.NONE)
     val queryFlow = MutableStateFlow("")
     val department = MutableStateFlow("")
-    private val _searchUsers = MutableStateFlow<Data>(Data.EmptyQuery)
 
     private val usersFlow = combine(
-        department,queryFlow
-    ){ department,query ->
-        Pair(department,query)
-    }.flatMapLatest {(department,query) -> repository.searchUsers(department,query)}
+        department, queryFlow
+    ) { department, query ->
+        Pair(department, query)
+    }.flatMapLatest { (department, query) -> repository.searchUsers(department, query) }
 
     val filter = usersFlow.asLiveData()
 
-    fun getUserFromDb() : LiveData<List<User>>{
-        return repository.getUsersByDep(department.value).asLiveData()
-    }
-    fun getUser(){
+    fun getUser() {
         viewModelScope.launch {
             repository.getUsers()
         }
+    }
+
+    fun isOnline(appCon: Context): Boolean {
+        val connMgr = appCon.applicationContext.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        return networkInfo?.isConnected == true
     }
 
     class Factory @Inject constructor(val api: Provider<UsersRepository>) : ViewModelProvider.Factory {
@@ -46,4 +48,6 @@ class UsersViewModel(val repository: UsersRepository): ViewModel() {
             return UsersViewModel(api.get()) as T
         }
     }
+
+
 }
